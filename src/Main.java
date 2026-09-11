8f0183571d6c0b88d2f3aaf96c2d65fceb7a604c
dharaneeT <tharandharanee12@gmail.com>
2026-09-11 18:22:59 +0530

    Day 1

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter your age: ");
+//        int age = sc.nextInt();
+//
+//        System.out.print("Enter your height: ");
+//        double height = sc.nextDouble();
+//
+//
+//        double ageInDouble = (double) age;
+//        int heightInt = (int) height;
+//
+//        int nextYearAge = age + 1;
+//
+//        int bonusYears = age + 5;
+//        int halfAge = age / 2;
+//
+//        System.out.println("Age after 5 years: " + bonusYears);
+//        System.out.println("Half of your age: " + halfAge);
+//
+//        sc.close();
+//    }
+//}
