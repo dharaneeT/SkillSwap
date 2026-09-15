85b76b7301443e818b571da03d80db790b99e3d0
dharaneeT <tharandharanee12@gmail.com>
2026-09-15 18:13:25 +0530

    Day 5

--- src/Backend/Main.java
+++ src/Backend/Main.java
@@ -1,55 +1,72 @@
-class User {
+//class InvalidSkillException extends RuntimeException {
+//    public InvalidSkillException(String message) {
+//        super(message);
+//    }
+//}
+//class SessionConflictException extends RuntimeException {
+//    public SessionConflictException(String message) {
+//        super(message);
+//    }
+//}
+//class InsufficientCreditsException extends RuntimeException {
+//    public InsufficientCreditsException(String message) {
+//        super(message);
+//    }
+//}
+//
 
-	private String name;
-	private int age;
-
-	User(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-
-	String getName() {
-		return name;
-	}
-
-	void setName(String name) {
-		this.name = name;
-	}
-gt
-	int getAge() {
-		return age;
-	}
-
-	void setAge(int age) {
-		this.age = age;
-	}
-}
-
-class Skill {
-
-	private String skillName;
-
-	Skill(String skillName) {
-		this.skillName = skillName;
-	}
-
-	String getSkillName() {
-		return skillName;
-	}
-}
-
-public class Day3 {
-
-	public static void main(String[] args) {
-		User u = new User("Dharanee", 21);
-		u.setAge(22);
-
-		Skill s = new Skill("Java");
-
-		System.out.println(u.getName() + " " + u.getAge());
-		System.out.println(s.getSkillName());
-	}
-}
+//class User {
+//
+//	private String name;
+//	private int age;
+//
+//	User(String name, int age) {
+//		this.name = name;
+//		this.age = age;
+//	}
+//
+//	String getName() {
+//		return name;
+//	}
+//
+//	void setName(String name) {
+//		this.name = name;
+//	}
+//gt
+//	int getAge() {
+//		return age;
+//	}
+//
+//	void setAge(int age) {
+//		this.age = age;
+//	}
+//}
+//
+//class Skill {
+//
+//	private String skillName;
+//
+//	Skill(String skillName) {
+//		this.skillName = skillName;
+//	}
+//
+//	String getSkillName() {
+//		return skillName;
+//	}
+//}
+//
+//public class Day3 {
+//
+//	public static void main(String[] args) {
+//		User u = new User("Dharanee", 21);
+//		u.setAge(22);
+//
+//		Skill s = new Skill("Java");
+//
+//		System.out.println(u.getName() + " " + u.getAge());
+//		System.out.println(s.getSkillName());
+//	}
+//}
 //class User {
 //    private String name;
 //    private int age;
