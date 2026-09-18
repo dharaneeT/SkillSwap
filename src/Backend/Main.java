1335726e5d3b3a9d4e510cb69cb60a44ff4516dd
dharaneeT <tharandharanee12@gmail.com>
2026-09-18 19:36:34 +0530

    Day 8

--- src/Backend/Main.java
+++ src/Backend/Main.java
@@ -1,21 +1,5 @@
+import java.util.function.Predicate;
 import java.util.*;
-
-class Repository<T> {
-
-	private List<T> data = new ArrayList<>();
-
-	public void add(T item) {
-		data.add(item);
-	}
-
-	public List<T> getAll() {
-		return data;
-	}
-
-	public void remove(T item) {
-		data.remove(item);
-	}
-}
 
 class User {
 
@@ -29,205 +13,274 @@ class User {
 		return name;
 	}
 }
+class MatchService {
+
+    public static Predicate<User> match(User currentUser) {
+
+        return otherUser ->
+                otherUser.skillsOffered.stream()
+                        .anyMatch(skill -> currentUser.skillsWanted.contains(skill));
+    }
+}
+
 
 public class Main {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		Repository<User> userRepo = new Repository<>();
+        User a = new User("Alice");
+        a.skillsWanted.add("Java");
+        a.skillsOffered.add("React");
 
-		userRepo.add(new User("DD"));
-		userRepo.add(new User("Dt"));
+        User b = new User("Bob");
+        b.skillsWanted.add("React");
+        b.skillsOffered.add("Java");
 
-		System.out.println(userRepo.getAll());
-	}
+        User c = new User("Charlie");
+        c.skillsOffered.add("Python");
+
+        List<User> users = Arrays.asList(a, b, c);
+
+        // 🔥 Predicate-based filtering
+        users.stream()
+                .filter(user -> user != a)
+                .filter(MatchService.match(a))
+                .forEach(user ->
+                        System.out.println(a.name + " matches with " + user.name)
+                );
+    }
 }
-//package Backend;
-//
+
+
 //import java.util.*;
 //
-//class User {
-//    String name;
-//    List<String> skillsOffered = new ArrayList<>();
-//    List<String> skillsWanted = new ArrayList<>();
+//class Repository<T> {
 //
-//    public User(String name) {
-//        this.name = name;
-//    }
+//	private List<T> data = new ArrayList<>();
+//
+//	public void add(T item) {
+//		data.add(item);
+//	}
+//
+//	public List<T> getAll() {
+//		return data;
+//	}
+//
+//	public void remove(T item) {
+//		data.remove(item);
+//	}
 //}
 //
+//class User {
 //
-//class SkillMatcher {
+//	String name;
 //
-//    List<User> users = new ArrayList<>();
+//	public User(String name) {
+//		this.name = name;
+//	}
 //
-//    void addUser(User user) {
-//        users.add(user);
-//    }
-//
-//    void matchUsers() {
-//
-//        for (User u1 : users) {
-//
-//            for (User u2 : users) {
-//
-//                if (u1 == u2) continue;
-//
-//                for (String skill : u1.skillsWanted) {
-//
-//                    if (u2.skillsOffered.contains(skill)) {
-//                        System.out.println(
-//                                u1.name + " matches with " + u2.name +
-//                                        " for skill: " + skill
-//                        );
-//                    }
-//                }
-//            }
-//        }
-//    }
+//	public String toString() {
+//		return name;
+//	}
 //}
 //
 //public class Main {
-//    public static void main(String[] args) {
 //
-//        SkillMatcher matcher = new SkillMatcher();
+//	public static void main(String[] args) {
+//		Repository<User> userRepo = new Repository<>();
 //
-//        User a = new User("Alice");
-//        a.skillsWanted.add("Java");
-//        a.skillsOffered.add("React");
+//		userRepo.add(new User("DD"));
+//		userRepo.add(new User("Dt"));
 //
-//        User b = new User("Bob");
-//        b.skillsWanted.add("React");
-//        b.skillsOffered.add("Java");
-//
-//        User c = new User("Charlie");
-//        c.skillsOffered.add("Python");
-//
-//        matcher.addUser(a);
-//        matcher.addUser(b);
-//        matcher.addUser(c);
-//
-//        matcher.matchUsers();
-//    }
+//		System.out.println(userRepo.getAll());
+//	}
 //}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-////class InvalidSkillException extends RuntimeException {
-////    public InvalidSkillException(String message) {
-////        super(message);
-////    }
-////}
-////class SessionConflictException extends RuntimeException {
-////    public SessionConflictException(String message) {
-////        super(message);
-////    }
-////}
-////class InsufficientCreditsException extends RuntimeException {
-////    public InsufficientCreditsException(String message) {
-////        super(message);
-////    }
-////}
+////package Backend;
 ////
-//
+////import java.util.*;
+////
 ////class User {
+////    String name;
+////    List<String> skillsOffered = new ArrayList<>();
+////    List<String> skillsWanted = new ArrayList<>();
 ////
-////	private String name;
-////	private int age;
-////
-////	User(String name, int age) {
-////		this.name = name;
-////		this.age = age;
-////	}
-////
-////	String getName() {
-////		return name;
-////	}
-////
-////	void setName(String name) {
-////		this.name = name;
-////	}
-////gt
-////	int getAge() {
-////		return age;
-////	}
-////
-////	void setAge(int age) {
-////		this.age = age;
-////	}
+////    public User(String name) {
+////        this.name = name;
+////    }
 ////}
 ////
-////class Skill {
 ////
-////	private String skillName;
+////class SkillMatcher {
 ////
-////	Skill(String skillName) {
-////		this.skillName = skillName;
-////	}
+////    List<User> users = new ArrayList<>();
 ////
-////	String getSkillName() {
-////		return skillName;
-////	}
-////}
-////
-////public class Day3 {
-////
-////	public static void main(String[] args) {
-////		User u = new User("Dharanee", 21);
-////		u.setAge(22);
-////
-////		Skill s = new Skill("Java");
-////
-////		System.out.println(u.getName() + " " + u.getAge());
-////		System.out.println(s.getSkillName());
-////	}
-////}
-////class User {
-////    private String name;
-////    private int age;
-////
-////    User(String name,int age){
-////        this.name=name;
-////        this.age=age;
+////    void addUser(User user) {
+////        users.add(user);
 ////    }
 ////
-////    String getName(){return name;}
-////    void setName(String name){this.name=name;}
+////    void matchUsers() {
 ////
-////    int getAge(){return age;}
-////    void setAge(int age){this.age=age;}
-////}
+////        for (User u1 : users) {
 ////
-////class Skill {
-////    private String skillName;
+////            for (User u2 : users) {
 ////
-////    Skill(String skillName){
-////        this.skillName=skillName;
+////                if (u1 == u2) continue;
+////
+////                for (String skill : u1.skillsWanted) {
+////
+////                    if (u2.skillsOffered.contains(skill)) {
+////                        System.out.println(
+////                                u1.name + " matches with " + u2.name +
+////                                        " for skill: " + skill
+////                        );
+////                    }
+////                }
+////            }
+////        }
 ////    }
-////
-////    String getSkillName(){return skillName;}
 ////}
 ////
-////public class Main{
+////public class Main {
 ////    public static void main(String[] args) {
-////        User u=new User("Dharanee",21);
-////        u.setAge(22);
 ////
-////        Skill s=new Skill("Java");
+////        SkillMatcher matcher = new SkillMatcher();
 ////
-////        System.out.println(u.getName()+" "+u.getAge());
-////        System.out.println(s.getSkillName());
+////        User a = new User("Alice");
+////        a.skillsWanted.add("Java");
+////        a.skillsOffered.add("React");
+////
+////        User b = new User("Bob");
+////        b.skillsWanted.add("React");
+////        b.skillsOffered.add("Java");
+////
+////        User c = new User("Charlie");
+////        c.skillsOffered.add("Python");
+////
+////        matcher.addUser(a);
+////        matcher.addUser(b);
+////        matcher.addUser(c);
+////
+////        matcher.matchUsers();
 ////    }
 ////}
+////
+////
+////
+////
+////
+////
+////
+////
+////
+////
+////
+////
+////
+////
+////
+////
+//////class InvalidSkillException extends RuntimeException {
+//////    public InvalidSkillException(String message) {
+//////        super(message);
+//////    }
+//////}
+//////class SessionConflictException extends RuntimeException {
+//////    public SessionConflictException(String message) {
+//////        super(message);
+//////    }
+//////}
+//////class InsufficientCreditsException extends RuntimeException {
+//////    public InsufficientCreditsException(String message) {
+//////        super(message);
+//////    }
+//////}
+//////
+////
+//////class User {
+//////
+//////	private String name;
+//////	private int age;
+//////
+//////	User(String name, int age) {
+//////		this.name = name;
+//////		this.age = age;
+//////	}
+//////
+//////	String getName() {
+//////		return name;
+//////	}
+//////
+//////	void setName(String name) {
+//////		this.name = name;
+//////	}
+//////gt
+//////	int getAge() {
+//////		return age;
+//////	}
+//////
+//////	void setAge(int age) {
+//////		this.age = age;
+//////	}
+//////}
+//////
+//////class Skill {
+//////
+//////	private String skillName;
+//////
+//////	Skill(String skillName) {
+//////		this.skillName = skillName;
+//////	}
+//////
+//////	String getSkillName() {
+//////		return skillName;
+//////	}
+//////}
+//////
+//////public class Day3 {
+//////
+//////	public static void main(String[] args) {
+//////		User u = new User("Dharanee", 21);
+//////		u.setAge(22);
+//////
+//////		Skill s = new Skill("Java");
+//////
+//////		System.out.println(u.getName() + " " + u.getAge());
+//////		System.out.println(s.getSkillName());
+//////	}
+//////}
+//////class User {
+//////    private String name;
+//////    private int age;
+//////
+//////    User(String name,int age){
+//////        this.name=name;
+//////        this.age=age;
+//////    }
+//////
+//////    String getName(){return name;}
+//////    void setName(String name){this.name=name;}
+//////
+//////    int getAge(){return age;}
+//////    void setAge(int age){this.age=age;}
+//////}
+//////
+//////class Skill {
+//////    private String skillName;
+//////
+//////    Skill(String skillName){
+//////        this.skillName=skillName;
+//////    }
+//////
+//////    String getSkillName(){return skillName;}
+//////}
+//////
+//////public class Main{
+//////    public static void main(String[] args) {
+//////        User u=new User("Dharanee",21);
+//////        u.setAge(22);
+//////
+//////        Skill s=new Skill("Java");
+//////
+//////        System.out.println(u.getName()+" "+u.getAge());
+//////        System.out.println(s.getSkillName());
+//////    }
+//////}
